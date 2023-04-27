1e1f468e1c948770b0bb4f4312938a764827ca11
hernan97carp <hernan97carp@hotmail.com>
2023-04-27 12:43:10 -0300

    commit

---
+++ express/express-restapi-crud/server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const morgan = require("morgan");
+const server = express();
+const PORT = 3001;
+let products = [
+  {
+    name: "libro",
+    price: 234,
+    id: 1,
+  },
+];
+server.use(morgan("dev"));
+server.use(express.json());
+server.get("/products", (req, res) => {
+  res.json(products);
+});
+
+server.post("/products", (req, res) => {
+  const newProduct = { ...req.body, id: products.length + 1 };
+  products.push(newProduct);
+  res.send(newProduct);
+});
+
+
+
+server.put("/products", (req, res) => {
+let newData = req.body
+
+  productsFound = products.find((p) => p.id === +req.params.id); //parseInt(req.params.id)
+  if (!productsFound)
+    res.status(404).json({
+      error: "No se encontro el producto",
+    });
+ products = products.map(p=> p.id === parseInt(req.params.id)?{...p, ...newData}:p)
+ 
+ res.json({
+  error: "Producto actualizado"
+ });
+});
+
+
+
+
+
+server.delete("/products/:id", (req, res) => {
+    productsFound = products.find((p) => p.id === +req.params.id); //parseInt(req.params.id)
+    if (!productsFound)
+      res.status(404).json({
+        error: "No se encontro el producto",
+      });
+   
+    products = products.filter(p=> p.id !== +req.params.id)
+    
+    console.log(products)
+    
+    
+    res.send("borrando producto");
+});
+
+server.get("/products/:id", (req, res) => {
+  console.log(req.params.id);
+  productsFound = products.find((p) => p.id === +req.params.id); //parseInt(req.params.id)
+  if (!productsFound)
+    res.status(404).json({
+      error: "No se encontro el producto",
+    });
+  res.json(productsFound);
+});
+server.listen(PORT, () => {
+  console.log(`server is in port ${PORT}`);
+});
